0ac65659cb79755bfac7df19900be9f6cdc1698d
Betolann <betolanp@hotmail.com>
2015-10-02 23:02:12 -0600

    Creación de la clase SelectionSort

---
+++ src/structbenchmark/SelectionSort.java
@@ -0,0 +1,22 @@
+package sort;
+
+public class SelectionSort {
+	public static void OrdenarSeleccion(int[] array){
+		
+
+			for(int i=0; i<array.length-1; i++){
+				int minIndex = i;
+				for(int j = i + 1; j<array.length; j++){
+					if(array[j]<array[minIndex]){
+						minIndex = j;
+		
+					}
+				}
+				
+				int	 temp = array[minIndex];
+				array[minIndex] = array[i];
+				array[i] = temp;	
+				
+			}		
+	}
+}
