16f788a3865f9a297bb935a28d5a0791d5703a17
Betolann <betolanp@hotmail.com>
2015-10-03 07:57:25 -0600

    Documentacion.

--- src/structbenchmark/BubbleSort.java
+++ src/structbenchmark/BubbleSort.java
@@ -2,26 +2,32 @@ package sort;
 
 public class BubbleSort {
 	
+	/**
+	 * Ordena el arreglo adecuadamente
+	 * @param array: El arreglo el cual desea ordenar.
+	 */
 	public void OrdenarBurbuja(int [] array) {
-		int aux;
-		boolean cambios = false;
+		int aux; // Variable Auxiliar
+		boolean cambios = false; // Cambia a verdadera cada vez que se efectua un cambio
 		
 		
 		while(true){
-			cambios = false;
-			for(int i=1; i<array.length; i++){
-				if(array[i]<array[i-1]){
+			cambios = false; 
+			for(int i=1; i<array.length; i++){ // i es el indice el cual recorrera el arreglo para realizar comparaciones
+				// Si el numero de la izquierda es mayor al de la derecha se realiza "swap"
+				if(array[i]<array[i-1]){ 
 					aux = array[i];
 					array[i] = array[i-1];
 					array[i-1] = aux;
-					cambios = true;
+					cambios = true; // Al ser verdadera indica que se realizo "swap" y permanece en el ciclo
 					
 				}
 			}
 			
-			if(cambios == false)
+			if(cambios == false) // Sino se realiza un cambio se detiene.
 				break;
 		}
 	}
 
 }
+
