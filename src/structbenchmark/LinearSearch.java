ea83c8e5e1e5817e93743be19c5e7da419ab3f10
Betolann <betolanp@hotmail.com>
2015-10-14 19:20:49 -0600

    Create LinearSearch.java

---
+++ src/structbenchmark/LinearSearch.java
@@ -0,0 +1,31 @@
+package sort;
+/**
+ * Se crea la clase LinearSearch
+ * @author Antony
+ * @version 08/10/15
+ */
+public class LinearSearch {
+	
+	/**
+	 * Se crea el método de busqueda lineal.
+	 * @param arreglo, pues es la estructura de datos donde se buscar.
+	 * @param elemento, pues es el dato a buscar, que esta dentro de la estructura de datos.
+	 * @return 
+	 */
+	    public static int linearsearch( int arreglo[], int elemento) {
+	        int i;
+	        boolean encontrado = false;
+	        
+	        for ( i = 0; i < arreglo.length && encontrado == false; i++) {
+	            
+	            if (elemento == arreglo[i]) {
+	                
+	                encontrado = true;
+	                return i;
+	            }
+	            
+	        } return -1;
+	    }
+
+
+}
